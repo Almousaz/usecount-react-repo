03c50fb0b3ee03696adcfb636b4d24a387009b1a
Ali Mousazada <mousazada.ali@gmail.com>
2024-05-07 21:00:08 -0400

    useing hook custom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useCount } from './useCount';
+
 
 function App() {
+
+  const {count , increase , decrease , reset} = useCount()
   return (
     <div className="App">
+      {count}
+
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+      <button onClick={reset}>reset</button>
       
 
 
